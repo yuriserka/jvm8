5e9691ac336c2eb2bab8689c14cd19ee55346e8e
Claudio Barros <dinhobarros15@gmail.com>
2019-12-02 06:29:38 -0300

    athrow implementada em seu caso mais básico, quando nao existem exception handlers disponíveis

--- include/utils/memory_areas/thread.h
+++ include/utils/memory_areas/thread.h
@@ -4,48 +4,48 @@
 #include "utils/memory_areas/java_stack.h"
 
 namespace MemoryAreas {
-
-class Heap;
-class MethodArea;
-
-class Thread {
- public:
-  Thread(MethodArea *method_area, Heap *heap, const ClassFile *cf) {
-    this->method_area = method_area;
-    this->heap = heap;
-    this->current_class = cf;
-    this->current_frame = nullptr;
-  }
-
-  void executeMethod(const std::string &method_name,
-                     const std::string &descriptor,
-                     const bool &popObjectRef = false);
-
-  void changeContext(const std::string &classname, const std::string &method,
-                     const std::string &arguments, const bool &popObjectRef);
-
-  void storeArguments(const std::string &args, Utils::Frame *new_frame,
-                      const bool &popObjectRef);
-
-  template <typename T>
-  void pushReturnValue(const T &val) {
-    auto curr_frame = this->jvm_stack.top();
-    this->jvm_stack.pop();
-    auto caller_frame = this->jvm_stack.top();
-    caller_frame->pushOperand<T>(val);
-    this->jvm_stack.push(curr_frame);
-  }
-
-  MethodArea *method_area;
-  Heap *heap;
-  Utils::Frame *current_frame;
-  const ClassFile *current_class;
-
- private:
-  JavaStack jvm_stack;
-
-  std::string current_method;
-};
+    
+    class Heap;
+    class MethodArea;
+    
+    class Thread {
+        public:
+        Thread(MethodArea *method_area, Heap *heap, const ClassFile *cf) {
+            this->method_area = method_area;
+            this->heap = heap;
+            this->current_class = cf;
+            this->current_frame = nullptr;
+        }
+        
+        void executeMethod(const std::string &method_name,
+                           const std::string &descriptor,
+                           const bool &popObjectRef = false);
+        
+        void changeContext(const std::string &classname, const std::string &method,
+                           const std::string &arguments, const bool &popObjectRef);
+        
+        void storeArguments(const std::string &args, Utils::Frame *new_frame,
+                            const bool &popObjectRef);
+        
+        template <typename T>
+            void pushReturnValue(const T &val) {
+            auto curr_frame = this->jvm_stack.top();
+            this->jvm_stack.pop();
+            auto caller_frame = this->jvm_stack.top();
+            caller_frame->pushOperand<T>(val);
+            this->jvm_stack.push(curr_frame);
+        }
+        
+        MethodArea *method_area;
+        Heap *heap;
+        Utils::Frame *current_frame;
+        const ClassFile *current_class;
+        
+        private:
+        JavaStack jvm_stack;
+        
+        std::string current_method;
+    };
 }  // namespace MemoryAreas
 
 #endif  // INCLUDE_UTILS_MEMORY_AREAS_THREAD_H_
--- src/instructions/instruction_set/reference.cc
+++ src/instructions/instruction_set/reference.cc
@@ -8,6 +8,7 @@
 #include "utils/object.h"
 
 namespace Instructions {
+<<<<<<< Updated upstream
 namespace Reference {
 /**
  * @brief Load reference from array
@@ -410,4 +411,239 @@ std::vector<int> Throw::execute(
 }
 // ----------------------------------------------------------------------------
 }  // namespace Reference
+=======
+    namespace Reference {
+        std::vector<int> LoadFromArray::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            int index = th->current_frame->popOperand<int>();
+            auto arrayref = th->current_frame->popOperand<Utils::Object *>()
+                ->data.as<Utils::Array_t *>();
+            th->current_frame->pushOperand(arrayref->get<Utils::Object *>(index));
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> StoreIntoArray::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            auto value = th->current_frame->popOperand<Utils::Object *>();
+            auto index = th->current_frame->popOperand<int>();
+            auto arrayref = th->current_frame->popOperand<Utils::Object *>()
+                ->data.as<Utils::Array_t *>();
+            arrayref->insert(value, index);
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> ConstNull::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            th->current_frame->pushOperand<Utils::Object>(nullptr);
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> Load::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            int localvar_index;
+            if (wide) {
+                localvar_index =
+                    static_cast<int16_t>((*++*code_iterator << 8) | *++*code_iterator);
+                *delta_code = 2;
+            } else {
+                localvar_index = int{*++*code_iterator};
+                *delta_code = 1;
+            }
+            th->current_frame->pushOperand(
+                th->current_frame->getLocalVarValue<Utils::Object *>(localvar_index));
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> Load_0::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            th->current_frame->pushOperand(
+                th->current_frame->getLocalVarValue<Utils::Object *>(0));
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> Load_1::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            th->current_frame->pushOperand(
+                th->current_frame->getLocalVarValue<Utils::Object *>(1));
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> Load_2::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            th->current_frame->pushOperand(
+                th->current_frame->getLocalVarValue<Utils::Object *>(2));
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> Load_3::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            th->current_frame->pushOperand(
+                th->current_frame->getLocalVarValue<Utils::Object *>(3));
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> NewArray::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            auto kpool_index = (*++*code_iterator << 8) | *++*code_iterator;
+            auto classname = th->method_area->runtime_constant_pool[kpool_index - 1]
+                .getClass<Utils::ConstantPool::CONSTANT_Class_info>()
+                ->getValue(th->method_area->runtime_constant_pool);
+            *delta_code = 2;
+            
+            auto count = th->current_frame->popOperand<int>();
+            
+            if (!count) {
+                throw Utils::Errors::JvmException(Utils::Errors::kNEGATIVEARRAYSIZE,
+                                                  "NegativeArraySizeException");
+            }
+            
+            auto arr = new Utils::Array_t(count, Utils::Reference::kREF_CLASS);
+            auto objectref =
+                new Utils::Object(arr, Utils::Reference::kREF_ARRAY, classname);
+            
+            th->current_frame->pushOperand(th->heap->pushReference(objectref));
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> Return::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            auto retval = th->current_frame->popOperand<Utils::Object *>();
+            th->pushReturnValue(retval);
+            th->current_frame->cleanOperands();
+            return {1};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> ArrayLength::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            auto arrayref = th->current_frame->popOperand<Utils::Object *>()
+                ->data.as<Utils::Array_t *>();
+            
+            th->current_frame->pushOperand(arrayref->length());
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> Store::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            int localvar_index;
+            if (wide) {
+                localvar_index =
+                    static_cast<int16_t>((*++*code_iterator << 8) | *++*code_iterator);
+                *delta_code = 2;
+            } else {
+                localvar_index = int{*++*code_iterator};
+                *delta_code = 1;
+            }
+            auto objectref = th->current_frame->popOperand<Utils::Object *>();
+            th->current_frame->pushLocalVar(objectref, localvar_index);
+            
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> Store_0::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            auto objectref = th->current_frame->popOperand<Utils::Object *>();
+            th->current_frame->pushLocalVar(objectref, 0);
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> Store_1::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            auto objectref = th->current_frame->popOperand<Utils::Object *>();
+            th->current_frame->pushLocalVar(objectref, 1);
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> Store_2::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            auto objectref = th->current_frame->popOperand<Utils::Object *>();
+            th->current_frame->pushLocalVar(objectref, 2);
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> Store_3::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            auto objectref = th->current_frame->popOperand<Utils::Object *>();
+            th->current_frame->pushLocalVar(objectref, 3);
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+        std::vector<int> Throw::execute(
+            std::vector<Utils::Types::u1>::iterator *code_iterator,
+            MemoryAreas::Thread *th, int *delta_code, const bool &wide, int *pc) {
+            if (Utils::Flags::options.kDEBUG) {
+                std::cout << "Executando " << Opcodes::getMnemonic(this->opcode) << "\n";
+            }
+            auto objectref = th->current_frame->popOperand<Utils::Object *>();
+            th->current_frame->cleanOperands();
+            th->current_frame->pushOperand(objectref);
+            
+            return {};
+        }
+        // ----------------------------------------------------------------------------
+    }  // namespace Reference
+>>>>>>> Stashed changes
 }  // namespace Instructions
